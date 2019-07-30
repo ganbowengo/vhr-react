adc8068ac2ddb68ecbdbe87c5cdf1afc3e879dba
“ganbowengo” <1245960973@qq.com>
2019-07-30 22:01:51 +0800

    引入 antd 组件库 配置按需引入

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg';  
+import { Button } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button type="primary" shape="circle" icon="search" />
+        <Button type="primary" icon="search">
+          Search
+        </Button>
+        <Button shape="circle" icon="search" />
+        <Button icon="search">Search</Button>
+        <br />
+        <Button shape="circle" icon="search" />
+        <Button icon="search">Search</Button>
+        <Button type="dashed" shape="circle" icon="search" />
+        <Button type="dashed" icon="search">
+          Search
+        </Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
